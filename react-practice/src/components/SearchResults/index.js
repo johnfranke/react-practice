f2c9468bcc27744cf9c75c7d88a24aa1d6f69e73
John Franke <johnrfrankejr@gmail.com>
2020-08-28 18:40:11 -0500

    updated breed search result styling

--- react-practice/src/components/SearchResults/index.js
+++ react-practice/src/components/SearchResults/index.js
@@ -5,9 +5,9 @@ function SearchResults(props) {
   return (
     <ul className="list-group search-results">
       {props.results.map(result => (
-        <li key={result} className="list-group-item">
+        <span key={result} className="list-group-item">
           <img alt="Dog" src={result} className="img-fluid" />
-        </li>
+        </span>
       ))}
     </ul>
   );
